cd8f6d1f92f8590f497814921e710e9c42a2b516
Karthik-1234567 <85018794+Karthik-1234567@users.noreply.github.com>
2021-06-27 20:27:59 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,28 @@
+void swap_max(int arr[],int l,int n){
+ 
+  int count = arr[n];
+  int sum = n;
+  int foo = arr[n];
+  for(int x =n;x<l;x++){
+    if(count<arr[x]){
+      count=arr[x];
+      sum = x;
+      }
+  }
+  arr[n]=arr[sum];
+  arr[sum]=foo;
+  
+  
+  
+  
+  }
+ void ssort(int arr[],int l){
+   
+   for(int x=0;x<l;x++){
+     swap_max(arr,l,x);
+     
+   }
+   
+ }
+ 
+
